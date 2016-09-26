f64b72a93ef00f2a0f20e2b54168aeddae8e8532
Alvaro Galan <alvaro.galgal@gmail.com>
2016-09-26 23:02:49 +0200

    FosUserBundle: Redefinición de form de registro y edición

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -19,9 +20,93 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $apellidos;
+
+    /**
+     * @ORM\Column(type="string", length=9, nullable=true)
+     */
+    protected $telefono;
+
     public function __construct()
     {
         parent::__construct();
-        // your own logic
+    }
+
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return User
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellidos
+     *
+     * @param string $apellidos
+     * @return User
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get apellidos
+     *
+     * @return string 
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set telefono
+     *
+     * @param string $telefono
+     * @return User
+     */
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    /**
+     * Get telefono
+     *
+     * @return string 
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
     }
 }
---
+++ src/AppBundle/Form/ProfileType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nombre');
+        $builder->add('apellidos');
+        $builder->add('telefono');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
+}
