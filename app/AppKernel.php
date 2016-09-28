264004bab59e2a44765df52017ddf73d83bbc517
Alvaro Galan <alvaro.galgal@gmail.com>
2016-09-28 23:37:49 +0200

    Añadido LadyBugBundle. Botón eliminar a la izquierda

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
