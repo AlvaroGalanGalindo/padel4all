09bb206b5ce10abe2473ba94445986f8d809ee5c
Álvaro Galán <alvaro.galan@solucionex.com>
2016-10-05 17:42:15 +0200

    Crud de Partido

---
+++ src/AppBundle/Form/PartidoType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PartidoType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fecha', 'datetime')
+            ->add('pista')
+            ->add('p1j1')
+            ->add('p1j2')
+            ->add('p2j1')
+            ->add('p2j2')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Partido'
+        ));
+    }
+}
