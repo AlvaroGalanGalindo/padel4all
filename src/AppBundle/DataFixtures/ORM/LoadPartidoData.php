d9c08b701fbc6c685246680813f21d405b01f39c
Alvaro Galan <alvaro.galgal@gmail.com>
2016-11-01 20:40:19 +0100

    Mejorados datafixtures de partidos

--- src/AppBundle/DataFixtures/ORM/LoadPartidoData.php
+++ src/AppBundle/DataFixtures/ORM/LoadPartidoData.php
@@ -25,12 +25,28 @@ class LoadPartidoData extends AbstractFixture implements OrderedFixtureInterface
         $this->faker->addProvider(new \Faker\Provider\Internet($this->faker));
     }
 
-    private function getData()
+    private function getData($i)
     {
         $entity = new Partido();
         $entity->setUser($this->getReference('USER'.rand(0,20)));
         $entity->setFecha($this->faker->dateTimeBetween($startDate = '+1 days', $endDate = '+1 months', $timezone = date_default_timezone_get()) );
         $entity->setPista($this->getReference("PISTA".rand(1,10)));
+
+        if ($i < 10) {
+            $fin = false;
+            $jugadores = [];
+            do {
+                $num = rand(1,10);
+                if (!in_array($num, $jugadores)) {
+                    $jugadores[] = $num;
+                    $fin = count($jugadores) == 4;
+                }
+            } while (!$fin);
+            $entity->setP1j1($this->getReference("USER".$jugadores[0]));
+            $entity->setP1j2($this->getReference("USER".$jugadores[1]));
+            $entity->setP2j1($this->getReference("USER".$jugadores[2]));
+            $entity->setP2j2($this->getReference("USER".$jugadores[3]));
+        }
 
         return $entity;
     }
@@ -41,7 +57,7 @@ class LoadPartidoData extends AbstractFixture implements OrderedFixtureInterface
     public function load(ObjectManager $manager)
     {
         for ($i=1; $i <= 10; $i++) {
-            $entity = $this->getData();
+            $entity = $this->getData($i);
             $this->setReference("partido".$i, $entity);
             $manager->persist($entity);
         }
